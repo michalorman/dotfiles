6e6dd06ddb8a7c831a0940d36ba539ef76f3f980
Michal Orman <michal.orman@gmail.com>
2022-01-08 13:33:20 +0100

    Added rofi run menu

---
+++ rofi/drunmenu.sh
@@ -0,0 +1,4 @@
+#!/bin/env bash
+
+rofi -no-config -no-lazy-grab -show drun -modi drun -drun-match-fields name,title -theme ~/.config/rofi/run.rasi
+
--- rofi/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/env bash
-
-rofi -no-config -no-lazy-grab -show drun -modi drun -drun-match-fields name -theme ~/.config/rofi/drun.rasi
-
